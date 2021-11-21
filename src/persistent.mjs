e3d923d18927cae06340a66ce04fc421c88ebff8
rpi <martin.hermant@gmail.com>
2021-11-21 08:02:33 +0100

    WIP

--- src/persistent.mjs
+++ src/persistent.mjs
@@ -1,55 +1,56 @@
 import path from 'path'
 import { execSync, execFileSync } from "child_process"
-import {readFileSync, writeFileSync} from 'fs'
-const proc =  execSync("uname -a").toString()
+import { readFileSync, writeFileSync } from 'fs'
+const proc = execSync("uname -a").toString()
 export const isPi = proc.includes("armv7")
-export const thisPath = isPi?"/home/pi/raspestrio/omxServer":"/home/tinmar/Dev/raspestrio/omxServer" 
-const confBasePath=thisPath+"/public/"
+export const thisPath = isPi ? "/home/pi/raspestrio/omxServer" : "/home/tinmar/Dev/raspestrio/omxServer"
+const confBasePath = thisPath + "/public/"
 const defaultConf = 'app.conf'
 
-export function loadConf(fName){
+export function loadConf(fName) {
     const confPath = confBasePath + (fName || defaultConf)
     try {
         const rawdata = readFileSync(confPath);
-        const conf= JSON.parse(rawdata);
-        console.log('loaded',conf,'from', confPath);
+        const conf = JSON.parse(rawdata);
+        console.log('loaded', conf, 'from', confPath);
         return conf
-        
+
     } catch (error) {
-        
+
     }
     return {}
 }
 
 
-export  function saveConf(conf,fName){
+export function saveConf(conf, fName) {
     const confPath = confBasePath + (fName || defaultConf)
     setRW(true)
     const jsonContent = JSON.stringify(conf);
-    console.log('saving',jsonContent,'in', confPath);
-    
+    console.log('saving', jsonContent, 'in', confPath);
+
     writeFileSync(confPath, jsonContent, 'utf8');
-    
+
     setRW(false)
-    
+
 }
 
-export function isRW(){
-    try{
-       const out = execSync("mount | grep 'type ext4' | grep rw")
-       if(out){return true}    
-    }catch(e){
-        
+export function isRW() {
+    try {
+        const out = execSync("mount | grep 'type ext4' | grep rw")
+        if (out) { return true }
+    } catch (e) {
+
     }
     return false
 }
+
 export const bootedInRW = isRW()
 
-export function setRW(isRW){
-    if(bootedInRW){
+export function setRW(isRW) {
+    if (bootedInRW) {
         console.log('ignoring rw as it was booted rw')
         return;
     }
-    const out =  execFileSync(thisPath+"/src/rw.sh",[isRW?"rw":"ro"])
-    if(out)console.log("rw out",out);
+    const out = execFileSync(thisPath + "/src/rw.sh", [isRW ? "rw" : "ro"])
+    if (out) console.log("rw out", out.toString());
 }
