83b34e69b121b27b54a80cc269bda58d50876dcb
tinmar <martin.hermant@gmail.com>
2022-11-15 18:44:35 +0100

    fix crash on stop

--- src/OMXPlayerCustom.js
+++ src/OMXPlayerCustom.js
@@ -6,8 +6,6 @@
 
 
 
-
-
 // ----- Requires ----- //
 
 let spawn = require('child_process').spawn;
@@ -24,7 +22,7 @@ let ALLOWED_OUTPUTS = ['hdmi', 'local', 'both', 'alsa'];
 // ----- Functions ----- //
 
 // Creates an array of arguments to pass to omxplayer.
-function buildArgs (source, givenOutput, loop, initialVolume, showOsd) {
+function buildArgs(source, givenOutput, loop, initialVolume, showOsd) {
 	let output = '';
 
 	if (givenOutput) {
@@ -44,7 +42,7 @@ function buildArgs (source, givenOutput, loop, initialVolume, showOsd) {
 		osd = showOsd;
 	}
 
-	let args = [source, '-o', output,  osd ? '' : '--no-osd'];
+	let args = [source, '-o', output, osd ? '' : '--no-osd'];
 
 	// Handle the loop argument, if provided
 	if (loop) {
@@ -63,7 +61,7 @@ function buildArgs (source, givenOutput, loop, initialVolume, showOsd) {
 
 // ----- Omx Class ----- //
 
-function Omx (source, output, loop, initialVolume, showOsd) {
+function Omx(source, output, loop, initialVolume, showOsd) {
 
 	// ----- Local Vars ----- //
 
@@ -74,7 +72,7 @@ function Omx (source, output, loop, initialVolume, showOsd) {
 	// ----- Local Functions ----- //
 
 	// Marks player as closed.
-	function updateStatus () {
+	function updateStatus() {
 
 		open = false;
 		omxplayer.emit('close');
@@ -82,7 +80,7 @@ function Omx (source, output, loop, initialVolume, showOsd) {
 	}
 
 	// Emits an error event, with a given message.
-	function emitError (message) {
+	function emitError(message) {
 
 		open = false;
 		omxplayer.emit('error', message);
@@ -90,7 +88,7 @@ function Omx (source, output, loop, initialVolume, showOsd) {
 	}
 
 	// Spawns the omxplayer process.
-	function spawnPlayer (src, out, loop, initialVolume, showOsd) {
+	function spawnPlayer(src, out, loop, initialVolume, showOsd) {
 
 		let args = buildArgs(src, out, loop, initialVolume, showOsd);
 		console.log('args for omxplayer:', args);
@@ -109,7 +107,7 @@ function Omx (source, output, loop, initialVolume, showOsd) {
 	}
 
 	// Simulates keypress to provide control.
-	function writeStdin (value) {
+	function writeStdin(value) {
 
 		if (open) {
 			player.stdin.write(value);
@@ -119,7 +117,7 @@ function Omx (source, output, loop, initialVolume, showOsd) {
 
 	// ----- Setup ----- //
 
-	function kill  (){
+	function kill() {
 		if (player) {
 			execSync('killall omxplayer && killall omxplayer.bin')
 			console.log("[Aplayer] killing")
@@ -136,7 +134,7 @@ function Omx (source, output, loop, initialVolume, showOsd) {
 
 
 	// ----- Methods ----- //
-	omxplayer.kill = ()=>{
+	omxplayer.kill = () => {
 		kill()
 	}
 	// Restarts omxplayer with a new source.
@@ -160,7 +158,7 @@ function Omx (source, output, loop, initialVolume, showOsd) {
 	omxplayer.volDown = () => { writeStdin('-'); };
 	omxplayer.fastFwd = () => { writeStdin('>'); };
 	omxplayer.rewind = () => { writeStdin('<'); };
-	omxplayer.fwd30 =() => { writeStdin('\u001b[C'); };
+	omxplayer.fwd30 = () => { writeStdin('\u001b[C'); };
 	omxplayer.back30 = () => { writeStdin('\u001b[D'); };
 	omxplayer.fwd600 = () => { writeStdin('\u001b[A'); };
 	omxplayer.back600 = () => { writeStdin('\u001b[B'); };
